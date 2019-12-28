27ae4b5775ba73bd11719c49c6be3bc930684d5c
Swapnil Tiwari <swapniltiwari775@gmail.com>
2019-12-28 20:25:23 +0530

    Upgraded to AndroidX

--- app/src/main/java/com/broooapps/otpedittext/MainActivity.java
+++ app/src/main/java/com/broooapps/otpedittext/MainActivity.java
@@ -1,6 +1,6 @@
 package com.broooapps.otpedittext;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
